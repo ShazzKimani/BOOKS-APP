325f9395f77d859b191c82b0864e1dfbea915b7d
Sharon Kimani <sharonkimani01@gmail.com>
2020-10-15 14:15:39 +0300

    Added README file

---
+++ README.md
@@ -0,0 +1,24 @@
+## Books-App
+This is an application that allows users to search technology books,and read them online
+
+### Author
+* Sharon Kimani
+
+### Requirements
+* Android Studio
+* Java Development Kit
+### Technologies Used
+* Java
+* ButterKnife
+### BDD
+User enters input, then app returns a list of books related to the search.
+
+### Getting Started
+* Clone the repo to your pc
+* Fire up Android Studio and make alterations however you want.
+### Contact
+* In case of anything reach via email at sharonkimani01@gmail.com
+
+
+Copyright (c) Sharon Kimani
+Licensed Under the MIT License
--- app/src/test/java/com/moringaschool/booksapp/ExampleUnitTest.java
+++ app/src/test/java/com/moringaschool/booksapp/ExampleUnitTest.java
@@ -2,6 +2,12 @@ package com.moringaschool.booksapp;
 
 import org.junit.Test;
 
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import org.junit.runner.RunWith;
+
 import static org.junit.Assert.*;
 
 /**
@@ -9,9 +15,13 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+@RunWith(AndroidJUnit4.class)
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.moringaschool.books_app", appContext.getPackageName());
     }
 }
+
+
