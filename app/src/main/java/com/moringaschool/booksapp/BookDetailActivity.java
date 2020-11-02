9ca2beb3d05a2255393e8bf113a3a8d2f826b5f1
Sharon Kimani <sharonkimani01@gmail.com>
2020-11-02 11:51:50 +0300

    CReated book detail fragment and added logic to it

---
+++ app/src/main/java/com/moringaschool/booksapp/BookDetailActivity.java
@@ -0,0 +1,25 @@
+package com.moringaschool.booksapp;
+
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.moringaschool.booksapp.databinding.ActivityBookDetailBinding;
+
+
+public class BookDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_detail);
+
+        Book book = getIntent().getParcelableExtra("Book");
+        ActivityBookDetailBinding binding = DataBindingUtil
+                .setContentView(this,R.layout.activity_book_detail);
+        binding.setBook(book);
+    }
+}
